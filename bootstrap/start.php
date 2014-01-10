0d17a41c122ca9f68fee82c977db97af7d063962
Maks Surguy <msurguy@gmail.com>
2014-01-10 12:48:02 -0800

    added localhost

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Maxoffsky-PC'),
+	'local' => array('localhost','Maxoffsky-PC'),
 
 ));
 
